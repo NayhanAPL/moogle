1e690ca1bf4d93e9dcc01491b777f8b7cba02d8f
Susana Becerra Pino <nayhanprovedolabrada@gmail.com>
2021-12-31 02:22:18 -0500

    hace el calculo y falta que revice todos los elementos no solo el primero

--- Program.cs
+++ Program.cs
@@ -13,24 +13,34 @@ namespace MoogleNY
         }
         public static Dictionary<string, int> RelevanciaDist(List<List<string>> listTxt, List<string> queryll, List<int> distancia)
         {
-
+            //tengo que implementar que revise todas las palabras no solo la primera que encuentra
             foreach (var txt in listTxt)
             {
                 int index = -1;
                 int pos = 0;
-                int ind = 0;
-                bool finCad = false;
+                int varPos = 0;
+                int rel = 10;
+                List<int> promedio = new List<int>();
                 int hasta = txt.Count - (index + 1);
                 for (int i = pos; i < queryll.Count; i++)
                 {
-                    
+                    if (pos == queryll.Count - 1) break;
                     index = txt.IndexOf(queryll[i], index + 1, hasta);
-                    if (index == -1 && finCad) { pos++; i = pos; hasta = txt.Count - (index + 1); }
-                    else 
+                    if (index == -1 && pos == i) { pos++; i = pos; hasta = txt.Count - (pos + 1); }
+                    if(index != -1) 
                     {
-                        hasta = (txt.Count - index) >= 10 ? 10 : txt.Count - (index + 1); 
-
+                        if (pos == i) varPos = index;
+                        int dist = distancia[i] - distancia[pos];
+                        if (pos != i)
+                        {
+                            int res = index - varPos > distancia[i] - distancia[pos] ? (index - varPos) - dist : dist - (index - varPos);
+                            if (res < rel) rel = res;
+                            index = varPos;
+                        }
+                        hasta = (txt.Count - index) >= 10 + dist ? 10 + dist : txt.Count - (index + 2); 
                     }
+                    if (i == queryll.Count - 1 && pos != queryll.Count - 1) 
+                    { pos++; i= pos; index= -1; hasta = txt.Count - (pos + 1);}
                 }
             }
             return new Dictionary<string, int>();
