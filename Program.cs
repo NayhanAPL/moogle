9e0c0f0b87cb796553d89ffeff417fb87038a28f
Susana Becerra Pino <nayhanprovedolabrada@gmail.com>
2022-01-02 12:44:23 -0500

    falta convertir res y rel en listas para evaluar independiente las distancias respecto a la palabra varPos

--- Program.cs
+++ Program.cs
@@ -10,18 +10,36 @@ namespace MoogleNY
     {
         static void Main(string[] args)
         {
+            List<string> queryll = new List<string>() { "a", "b", "c" };
+            List<int> distancia = new List<int>() { 0,1,3};
+            List<string> nombresTxt = new List<string>() { "txt1", "txt2", "txt3", "txt4", "txt5", "txt6", };
+            List<string> Txt1 = new List<string>() { "j", "a", "s", "f", "c", "a", };
+            List<string> Txt2 = new List<string>() { "a", "b", "c", "c", "u", "j", };
+            List<string> Txt3 = new List<string>() { "j", "m", "d", "n", "x", "v", };
+            List<string> Txt4 = new List<string>() { "v", "b", "c", "f", "z", "h", };
+            List<string> Txt5 = new List<string>() { "x", "n", "s", "d", "s", "j", };
+            List<string> Txt6 = new List<string>() { "a", "x", "s", "f", "z", "b", };
+            List<List<string>> listTxt = new List<List<string>>() { Txt1, Txt2, Txt3, Txt4, Txt5, Txt6 };
+
+            var x = RelevanciaDist(nombresTxt, listTxt, queryll, distancia);
+            foreach (var item in x)
+            {
+                Console.WriteLine(item.Key + "  " + item.Value);
+            }
+           
         }
-        public static Dictionary<string, double> RelevanciaDist(List<string> nombresTxt, List<List<string>> listTxt, List<string> queryll, List<int> distancia)
+        public static Dictionary<double, string> RelevanciaDist(List<string> nombresTxt, List<List<string>> listTxt, List<string> queryll, List<int> distancia)
         {
             int contTxt = 0;
-            Dictionary<string, double> ordenado = new Dictionary<string, double>();
+            Dictionary<double, string> resultados = new Dictionary<double, string>();
             foreach (var txt in listTxt)
             {
                 contTxt++;
                 int index = -1;
                 int pos = 0;
                 int varPos = 0;
-                int rel = 10;
+                List<int> rel = new List<int>(queryll.Count - 1); for (int i = 0; i < rel.Count; i++) { rel[i] = 10; }
+          
                 List<int> promedio = new List<int>();
                 int hasta = txt.Count - (index + 1);
                 for (int i = pos; i < queryll.Count; i++)
@@ -35,19 +53,26 @@ namespace MoogleNY
                         int dist = distancia[i] - distancia[pos];
                         if (pos != i)
                         {
-                            int res = index - varPos > distancia[i] - distancia[pos] ? (index - varPos) - dist : dist - (index - varPos);
+                            List<int> res = new List<int>(queryll.Count-1);
+                            res[0] = index - varPos > distancia[i] - distancia[pos] ? (index - varPos) - dist : dist - (index - varPos);
                             if (res < rel) rel = res;
-                            index = varPos;
+                            index = varPos; 
                         }
                         hasta = (txt.Count - index) >= 10 + dist ? 10 + dist : txt.Count - (index + 2); 
                     }
+                    if (index == -1 && pos != i) index = varPos;
                     if (i == queryll.Count - 1 && pos != queryll.Count - 1) 
-                    { i= pos-1; index= varPos; hasta = txt.Count - (varPos + 1);}
+                    { i= pos-1; index= varPos; hasta = txt.Count - (varPos + 2);}
                 }
-                int suma = 0;
-                foreach (var num in promedio) { suma += num; }
-                double orden = suma / promedio.Count;
-                ordenado.Add(nombresTxt[contTxt], orden);
+                double orden = promedio.Average();
+                resultados.Add(orden, nombresTxt[contTxt]);
+                
+            }
+            Dictionary<double, string> ordenado = new Dictionary<double, string>();
+            for (int i = 0; i < nombresTxt.Count; i++)
+            {
+                ordenado.Add(resultados.Max().Key, resultados.Max().Value);
+                resultados.Remove(resultados.Max().Key);
             }
             
             return ordenado;
