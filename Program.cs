abafd9339372244f5d1f40d373b07c238992249b
Susana Becerra Pino <nayhanprovedolabrada@gmail.com>
2022-01-02 15:49:50 -0500

    Cambio total del codugo, terminado el metodo Distancia(nombresTxt, ListaDiccionario(palabras, indices), queryll, distancia)

--- Program.cs
+++ Program.cs
@@ -13,69 +13,57 @@ namespace MoogleNY
             List<string> queryll = new List<string>() { "a", "b", "c" };
             List<int> distancia = new List<int>() { 0,1,3};
             List<string> nombresTxt = new List<string>() { "txt1", "txt2", "txt3", "txt4", "txt5", "txt6", };
-            List<string> Txt1 = new List<string>() { "j", "a", "s", "f", "c", "a", };
-            List<string> Txt2 = new List<string>() { "a", "b", "c", "c", "u", "j", };
-            List<string> Txt3 = new List<string>() { "j", "m", "d", "n", "x", "v", };
-            List<string> Txt4 = new List<string>() { "v", "b", "c", "f", "z", "h", };
-            List<string> Txt5 = new List<string>() { "x", "n", "s", "d", "s", "j", };
-            List<string> Txt6 = new List<string>() { "a", "x", "s", "f", "z", "b", };
-            List<List<string>> listTxt = new List<List<string>>() { Txt1, Txt2, Txt3, Txt4, Txt5, Txt6 };
+            Dictionary<string, List<int>> Txt1 = new Dictionary<string, List<int>>();
+            Txt1.Add("a", new List<int>() { 4, 56, 242, 255, 534, 711 });
+            Txt1.Add("b", new List<int>() { 7, 59, 246, 256, 554, 723 });
+            Txt1.Add("c", new List<int>() { 23, 74, 286, 226, 600, 773 });
+            Dictionary<string, List<int>> Txt2 = new Dictionary<string, List<int>>();
+            Txt2.Add("a", new List<int>() { 4, 56, 242, 255, 534, 711 });
+            Txt2.Add("b", new List<int>() { 34, 69, 256, 266, 584, 743 });
+            Txt2.Add("c", new List<int>() { 47, 81, 276, 286, 600, 773 });
+            Dictionary<string, List<int>> Txt3 = new Dictionary<string, List<int>>();
+            Txt3.Add("a", new List<int>() { 4, 56, 242, 255, 534, 711 });
+            Txt3.Add("b", new List<int>() { 5, 57, 243, 256, 554, 723 });
+            Txt3.Add("c", new List<int>() { 7, 59, 286, 226, 600, 773 });
+            List<Dictionary<string, List<int>>> listTxt = new List<Dictionary<string, List<int>>>() { Txt1, Txt2, Txt3};
 
-            var x = RelevanciaDist(nombresTxt, listTxt, queryll, distancia);
+            var x = Distancia(nombresTxt, listTxt, queryll, distancia);
             foreach (var item in x)
             {
                 Console.WriteLine(item.Key + "  " + item.Value);
             }
-           
+            Console.ReadLine();
         }
-        public static Dictionary<double, string> RelevanciaDist(List<string> nombresTxt, List<List<string>> listTxt, List<string> queryll, List<int> distancia)
+        public static Dictionary<double, string> Distancia(List<string> nombresTxt, List<Dictionary<string, List<int>>> listDiccionary, List<string> queryll, List<int> distancia)
         {
-            int contTxt = 0;
             Dictionary<double, string> resultados = new Dictionary<double, string>();
-            foreach (var txt in listTxt)
+            int contTxt = 0;
+            foreach (var diccionario in listDiccionary)
             {
-                contTxt++;
-                int index = -1;
-                int pos = 0;
-                int varPos = 0;
-                List<int> rel = new List<int>(queryll.Count - 1); for (int i = 0; i < rel.Count; i++) { rel[i] = 10; }
-          
                 List<int> promedio = new List<int>();
-                int hasta = txt.Count - (index + 1);
-                for (int i = pos; i < queryll.Count; i++)
+
+                for (int i = 0; i < queryll.Count-1; i++)
                 {
-                    if (pos == queryll.Count - 1) break;
-                    index = txt.IndexOf(queryll[i], index + 1, hasta);
-                    if (index == -1 && pos == i) { pos++; i = pos-1; hasta = txt.Count - (varPos + 1); promedio.Add(10-rel); rel = 10; }
-                    if(index != -1) 
+                    for (int j = i+1; j < queryll.Count; j++)
                     {
-                        if (pos == i) varPos = index;
-                        int dist = distancia[i] - distancia[pos];
-                        if (pos != i)
+                        int rel = 10;
+                        int dif = distancia[j] - distancia[i];
+                        foreach (var indexI in diccionario[queryll[i]])
                         {
-                            List<int> res = new List<int>(queryll.Count-1);
-                            res[0] = index - varPos > distancia[i] - distancia[pos] ? (index - varPos) - dist : dist - (index - varPos);
-                            if (res < rel) rel = res;
-                            index = varPos; 
+                            foreach (var indexJ in diccionario[queryll[j]])
+                            {
+                                int res = indexJ - indexI > dif ? (indexJ - indexI) - dif : dif - (indexJ - indexI);
+                                if (res < rel) rel = res;
+                            }
                         }
-                        hasta = (txt.Count - index) >= 10 + dist ? 10 + dist : txt.Count - (index + 2); 
+                        promedio.Add(10-rel);
                     }
-                    if (index == -1 && pos != i) index = varPos;
-                    if (i == queryll.Count - 1 && pos != queryll.Count - 1) 
-                    { i= pos-1; index= varPos; hasta = txt.Count - (varPos + 2);}
                 }
-                double orden = promedio.Average();
-                resultados.Add(orden, nombresTxt[contTxt]);
-                
+                double pro = promedio.Average();
+                resultados.Add(pro, nombresTxt[contTxt]); contTxt++;
             }
-            Dictionary<double, string> ordenado = new Dictionary<double, string>();
-            for (int i = 0; i < nombresTxt.Count; i++)
-            {
-                ordenado.Add(resultados.Max().Key, resultados.Max().Value);
-                resultados.Remove(resultados.Max().Key);
-            }
-            
-            return ordenado;
+            resultados.OrderByDescending(result => result.Key);
+            return resultados;
         }
     }
 }
